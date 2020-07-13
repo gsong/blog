2646feea7cb9d6d7fa7346301f1152fcb0759b19
George Song <george@damacy.net>
2020-07-12 21:32:14 -0700

    feat: don't automatically load sandboxes

--- src/components/CodeSandbox.js
+++ src/components/CodeSandbox.js
@@ -4,6 +4,7 @@ import { color } from "../styles";
 import { isBrowser, isIE11 } from "../utils";
 
 const CodeSandbox = ({ as: As = "h2", sandbox, title, ...props }) => {
+  const [shouldLoad, setShouldLoad] = React.useState(false);
   const url = `https://codesandbox.io/s/${sandbox}`;
 
   return isIE11 ? (
@@ -13,19 +14,27 @@ const CodeSandbox = ({ as: As = "h2", sandbox, title, ...props }) => {
     </p>
   ) : (
     <>
-      <As>Try It Out for Yourself</As>
-      <p>
-        <a href={url}>Play with the live code at CodeSandbox</a>.
-      </p>
-      <iframe
-        src={`https://codesandbox.io/embed/${sandbox}?${queryParams}`}
-        title={`CodeSandbox: ${title}`}
-        css={style}
-        loading="lazy"
-        allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
-        sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-        {...props}
-      ></iframe>
+      <As>👩‍💻 Try It Out for Yourself</As>
+      {shouldLoad ? (
+        <>
+          <p>
+            <a href={url}>Play with the live code at CodeSandbox</a>.
+          </p>
+          <iframe
+            src={`https://codesandbox.io/embed/${sandbox}?${queryParams}`}
+            title={`CodeSandbox: ${title}`}
+            css={style}
+            loading="lazy"
+            allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+            sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+            {...props}
+          ></iframe>
+        </>
+      ) : (
+        <p>
+          <button onClick={() => setShouldLoad(true)}>Load CodeSandbox</button>
+        </p>
+      )}
     </>
   );
 };
@@ -55,8 +64,8 @@ const queryParams = Object.entries(options())
 
 const style = {
   border: 0,
-  boxShadow: `0 0 0 4px ${color.yellow[1]}`,
-  height: "clamp(150px, 30vh, 500px)",
+  outline: `4px solid ${color.yellow[1]}`,
+  height: "clamp(150px, 60vh, 800px)",
   overflow: "hidden",
 
   // breakout of parent
