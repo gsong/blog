2f2d3542d55dc9ec15acf16fe25cf9374ea53b1f
George Song <george@damacy.net>
2020-07-24 14:16:11 -0700

    fix: no need to constrain banner max width

--- src/pages/helpers/Home.js
+++ src/pages/helpers/Home.js
@@ -19,11 +19,10 @@ const Header = () => {
     <header sx={{ alignItems: "center", display: "flex", mt: 4, mb: 7, mx: 0 }}>
       <h1
         sx={{
-          fontSize: `clamp(${theme.fontSizes.base}, 7vw, ${theme.fontSizes.xl3})`,
+          fontSize: `clamp(${theme.fontSizes.base}, 6.8vw, ${theme.fontSizes.xl3})`,
           fontWeight: 700,
           hyphens: "auto",
           margin: `0 0 0 ${theme.space[3]}`,
-          maxWidth: 350,
           order: 1,
 
           // stylelint-disable-next-line media-feature-name-case
